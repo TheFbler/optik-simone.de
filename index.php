b137661d0e13371159189b769eda75d535ca5c01
Fabian Clasen <fabi.clasen@googlemail.com>
2020-05-01 14:26:26 +0200

    Lazysizes hinzufügen

--- index.php
+++ index.php
@@ -218,5 +218,6 @@
     <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
     <script type="text/javascript" src="js/mobile-menu.js"></script>
     <script type="text/javascript" src="js/script.js"></script>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
   </body>
 </html>
